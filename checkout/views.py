dba344b16f72666931d90befcf0beab53f1905bf
JoelMichaelRutter <joel-rutter@live.co.uk>
2022-01-18 22:19:57 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KJDfbJf8l8PCYvAk7trgw6o5xRXFZGYslJKN47AtjW54YHum2YCoMQ5DAmWxtJeUvZE0wt0jvn8ap93RCR9iaDw00nBoFPuBz',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
