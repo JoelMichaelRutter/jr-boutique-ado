5523ea5dfed44396ce1d962aa5717995ce1a562e
JoelMichaelRutter <joel-rutter@live.co.uk>
2022-01-18 09:10:48 +0000

    add checkout app template, view and url

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -46,6 +46,11 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+
+    # other
+    'crispy_forms',
+
 ]
 
 MIDDLEWARE = [
@@ -59,6 +64,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'boutique_ado.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -74,8 +81,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by all auth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
